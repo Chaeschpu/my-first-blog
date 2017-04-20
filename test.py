b4d4ea9b82bbba935e09ebb611f16095916d8db4
Chaeschpu <caspar.schucan@base4kids.ch>
2017-04-20 09:23:45 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+def hi(name):
+    if name == 'Caspar':
+        print('Hallo Caspar!')
+    elif name == 'Herr Schucan':
+        print('Guten Tag Herr Schucan!')
+    else:
+        print('Hallo! Wer bist du') 
+hi('Herr Schucan')
+        
